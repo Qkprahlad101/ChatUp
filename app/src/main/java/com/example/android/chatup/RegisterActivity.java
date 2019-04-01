ce80b83b750a79b116b9dd9729a5b7eb57662ac8
Prahlad Kumar <k.prahlad101@gmail.com>
2019-04-01 12:18:47 +0530

    2nd C and Push

--- app/src/main/java/com/example/android/chatup/RegisterActivity.java
+++ app/src/main/java/com/example/android/chatup/RegisterActivity.java
@@ -1,13 +1,74 @@
 package com.example.android.chatup;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    MaterialEditText username,email,password;
+    Button tbn_register;
+
+    FirebaseAuth auth;
+    DatabaseReference reference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        tbn_register = findViewById(R.id.tbn_register);
+
+        auth = FirebaseAuth.getInstance();
+    }
+
+    private void register(final String username ,String email,String password){
+
+        auth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            String userid = firebaseUser.getUid();
+
+                            reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+
+                            HashMap<String,String> hashmap= new HashMap<>();
+                            hashmap.put("id",userid);
+                            hashmap.put("username",username);
+                            hashmap.put("imageUrl","default");
+
+
+                            reference.setValue(hashmap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Intent intent = new Intent(RegisterActivity.this,)
+                                    }
+                                }
+                            })
+
+                        }
+                    }
+                })
     }
 }
